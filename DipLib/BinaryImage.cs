2607ec06c305f298b3b060956c897167571fc218
HowyoungZhou <howyoung3@gmail.com>
2019-10-17 21:25:24 +0800

    Fix the bug in the implementation of Otsu's method

--- DipLib/BinaryImage.cs
+++ DipLib/BinaryImage.cs
@@ -18,9 +18,18 @@ namespace DipLib
             Pixels = pixels;
         }
 
-        public BinaryImage(byte[] grayScalePixels)
+        public BinaryImage(byte[] grayScalePixels, int pixelWidth, int pixelHeight)
         {
-
+            double threshold = GetThresholdByOtsu(grayScalePixels);
+            Pixels = new byte[pixelWidth, pixelHeight];
+            for (int x = 0; x < pixelWidth; x++)
+            {
+                for (int y = 0; y < pixelHeight; y++)
+                {
+                    byte pixel = grayScalePixels[y * pixelWidth + x];
+                    Pixels[x, y] = (byte)(pixel > threshold ? 255 : 0);
+                }
+            }
         }
 
         private static int[] GetGrayScaleHistogram(byte[] grayScalePixels)
@@ -33,21 +42,62 @@ namespace DipLib
             return res;
         }
 
-        private static int GetThresholdByOtsu(byte[] grayScalePixels)
+        //private static double GetThresholdByOtsu(byte[] grayScalePixels)
+        //{
+        //    int pixelsCount = grayScalePixels.Length;
+        //    int[] histogram = GetGrayScaleHistogram(grayScalePixels);
+
+        //    int classSumTotal = 0;
+        //    for (int i = 1; i < 256; i++)
+        //    {
+        //        classSumTotal += histogram[i] * i;
+        //    }
+
+        //    int classProbLeft = 0;
+        //    int classSumLeft = 0;
+        //    double maxInnerVar = 0;
+        //    int threshold1 = 0, threshold2 = 0;
+
+        //    for (int threshold = 0; threshold < 256; threshold++)
+        //    {
+        //        int probability = histogram[threshold];
+        //        classProbLeft += probability;
+        //        if (classProbLeft == 0) continue;
+
+        //        int classProbRight = pixelsCount - classProbLeft;
+        //        if (classProbRight == 0) break;
+
+        //        classSumLeft += threshold * probability;
+        //        double classMeanLeft = (double)classSumLeft / classProbLeft;
+        //        double classMeanRight = (double)(classSumTotal - classSumLeft) / classProbRight;
+        //        double innerClassVar = classProbLeft * classProbRight * (classMeanLeft - classMeanRight) * (classMeanLeft - classMeanRight);
+        //        if (innerClassVar >= maxInnerVar)
+        //        {
+        //            threshold1 = threshold;
+        //            if (innerClassVar > maxInnerVar)
+        //            {
+        //                threshold2 = threshold;
+        //            }
+        //            maxInnerVar = innerClassVar;
+        //        }
+        //    }
+
+        //    return (threshold1 + threshold2) / 2.0;
+        //}
+
+        private static double GetThresholdByOtsu(byte[] grayScalePixels)
         {
             int pixelsCount = grayScalePixels.Length;
             int[] histogram = GetGrayScaleHistogram(grayScalePixels);
 
             int classSumTotal = 0;
-            for (int i = 0; i < 256; i++)
-            {
-                classSumTotal += histogram[i] * i;
-            }
+            for (int i = 1; i < 256; i++) classSumTotal += i * histogram[i];
 
-            int classProbLeft = 0;
             int classSumLeft = 0;
+            int classProbLeft = 0;
             double maxInnerVar = 0;
-            int threshold1 = 0, threshold2 = 0;
+            double threshold1 = 0;
+            double threshold2 = 0;
 
             for (int threshold = 0; threshold < 256; threshold++)
             {
@@ -56,26 +106,33 @@ namespace DipLib
                 if (classProbLeft == 0) continue;
 
                 int classProbRight = pixelsCount - classProbLeft;
-                if (classProbRight == 0) break;
+                if (classProbRight == 0) continue;
 
                 classSumLeft += threshold * probability;
-                double classMeanLeft = classSumLeft / classProbLeft;
-                double classMeanRight = (classSumTotal - classSumLeft) / classProbRight;
-                double innerClassVar = classProbLeft * classProbRight * (classMeanLeft - classMeanRight) * (classMeanLeft - classMeanRight);
+                int classMeanDiff = classSumLeft / classProbLeft - (classSumTotal - classSumLeft) / classProbRight;
+                double innerClassVar = classProbLeft * classProbRight * classMeanDiff * classMeanDiff;
+
                 if (innerClassVar >= maxInnerVar)
                 {
                     threshold1 = threshold;
-                    if (innerClassVar > maxInnerVar)
-                    {
-                        threshold2 = threshold;
-                    }
+                    if (innerClassVar > maxInnerVar) threshold2 = threshold;
                     maxInnerVar = innerClassVar;
                 }
             }
-
             return (threshold1 + threshold2) / 2;
         }
+
+        public byte[] ToPixelsData()
+        {
+            byte[] data = new byte[PixelHeight * PixelWidth];
+            for (int x = 0; x < PixelWidth; x++)
+            {
+                for (int y = 0; y < PixelHeight; y++)
+                {
+                    data[y * PixelWidth + x] = Pixels[x, y];
+                }
+            }
+            return data;
+        }
     }
-
-
 }
